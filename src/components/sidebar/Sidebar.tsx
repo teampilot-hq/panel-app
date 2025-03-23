bb9ab89c572b1b743e9b302ffff3627e5fe63b8f
Rozita Hasani <106513953+rozita-hasani@users.noreply.github.com>
2025-03-23 18:57:06 +0100

    feat: add search command to toolbar (#93)

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,29 +1,16 @@
-import React, {useContext, useState} from 'react';
+import React, {useContext} from 'react';
 import {Link, useLocation} from 'react-router-dom';
 import {UserContext} from "@/contexts/UserContext.tsx";
 import SidebarAccountDropdown from "@/components/sidebar/SidebarAccountDropdown.tsx";
 import Logo from "@/components/icon/Logo.tsx";
 import {NotificationBell} from "@/modules/notification/components/NotificationBell.tsx";
-import {UserRole} from "@/core/types/enum.ts";
-import {NavigationItem, navigationItems} from "@/core/types/NavigationItem.ts";
-
-const getAccessibleNavigationItems = (role: UserRole): NavigationItem[] => {
-    return navigationItems.filter(item => item.accessLevel.includes(role))
-}
+import {NavigationItem} from "@/core/types/navigationItem.ts";
+import {getAccessibleNavigationItems} from "@/core/utils/navigation.ts";
 
 export default function Sidebar() {
-    const [selectedOption, setSelectedOption] = useState<string>('');
     const location = useLocation();
     const {user} = useContext(UserContext);
     const accessibleItems = user ? getAccessibleNavigationItems(user.role) : [];
-
-    const handleNavigation = (path: string) => {
-        setSelectedOption(path);
-    };
-
-    const isNavigationActive = (path: string) => {
-        return location.pathname === path || selectedOption === path;
-    };
 
     return (
         <div className="left-0 hidden md:block md:w-[240px] lg:w-[280px]">
@@ -37,9 +24,9 @@ export default function Sidebar() {
                         <NotificationBell/>
                     </div>
 
-                    <NavigationSection items={accessibleItems} isNavigationActive={isNavigationActive} onClick={handleNavigation}/>
+                    <NavigationSection items={accessibleItems} currentPath={location.pathname}/>
 
-                    <SidebarAccountDropdown isActive={isNavigationActive('/profile')} onClick={handleNavigation}/>
+                    <SidebarAccountDropdown isActive={location.pathname === '/profile'} />
                 </div>
             </div>
         </div>
@@ -49,26 +36,35 @@ export default function Sidebar() {
 type NavigationSectionProps = {
     title?: string;
     items: NavigationItem[];
-    isNavigationActive: (path: string) => boolean;
-    onClick: (page: string) => void;
-}
+    currentPath: string;
+};
 
-function NavigationSection({title, items, isNavigationActive, onClick}: NavigationSectionProps) {
+function NavigationSection({ title, items, currentPath }: NavigationSectionProps) {
     return (
         <div className="flex-1 overflow-y-auto px-4">
             <h2 className="px-3 text-xs font-semibold text-gray-500 uppercase tracking-wider my-3">{title}</h2>
-            {items.map((item) => (
-                <Link
-                    key={item.title}
-                    to={item.path}
-                    onClick={() => onClick(item.path)}
-                    className={`group flex items-center gap-3 rounded-lg px-3 py-2 text-sm font-medium transition-colors ${
-                        isNavigationActive(item.path) ? "bg-indigo-50 text-indigo-600" : "text-gray-600 hover:bg-gray-50 hover:text-gray-900"}`}
-                >
-                    <item.icon className={`h-5 w-5 ${isNavigationActive(item.path) ? "text-indigo-600" : "text-gray-400 group-hover:text-gray-500"}`}/>
-                    {item.title}
-                </Link>
-            ))}
+            {items.map((item) => {
+                const isActive = currentPath === item.path;
+
+                return (
+                    <Link
+                        key={item.title}
+                        to={item.path}
+                        className={`group flex items-center gap-3 rounded-lg px-3 py-2 text-sm font-medium transition-colors ${
+                            isActive
+                                ? "bg-indigo-50 text-indigo-600"
+                                : "text-gray-600 hover:bg-gray-50 hover:text-gray-900"
+                        }`}
+                    >
+                        <item.icon
+                            className={`h-5 w-5 ${
+                                isActive ? "text-indigo-600" : "text-gray-400 group-hover:text-gray-500"
+                            }`}
+                        />
+                        {item.title}
+                    </Link>
+                );
+            })}
         </div>
     );
 }
